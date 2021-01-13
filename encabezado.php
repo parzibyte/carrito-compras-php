3a2eabfee7781c4974918c7c5091b7466e5e7c78
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-13 13:28:32 -0600

    Título

--- encabezado.php
+++ encabezado.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Plantilla inicial de Bulma CSS - By Parzibyte</title>
+    <title>Carrito de compras con PHP y MySQL - By Parzibyte</title>
     <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.1/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
 </head>
